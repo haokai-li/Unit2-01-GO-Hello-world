82936fb73853cc8415c289b5b708f53d1d88ed20
haokaili <>
2021-05-04 04:02:36 +0000

    added hello world

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Unit2-01-GO-Hello-world
+[![GitHub's Super Linter](https://github.com/haokai-li/Unit2-01-GO-Hello-world/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/haokai-li/Unit2-01-GO-Hello-world/actions)
---
+++ helloworld.go
@@ -0,0 +1,13 @@
+// Created by: haokai
+// Created on: Apr 2021
+
+// This program displays, "hello, world!"
+
+package main
+
+import "fmt"
+
+func main() {
+  // This function displays, "hello, world"
+  fmt.Println("Hello, world!")
+}
